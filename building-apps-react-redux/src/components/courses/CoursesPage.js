ec3f88d1fc3107e662559696ebf85a9288adeb44
gemmas95 <gemma1030@gmail.com>
2020-09-30 15:41:09 +0200

    redux flow done

--- building-apps-react-redux/src/components/courses/CoursesPage.js
+++ building-apps-react-redux/src/components/courses/CoursesPage.js
@@ -1,4 +1,9 @@
 import React from "react";
+import { connect } from "react-redux";
+// The connect function connects our component to redux
+import * as courseActions from "../../redux/actions/courseActions";
+import PropTypes from "prop-types";
+import { bindActionCreators } from "redux";
 
 class CoursesPage extends React.Component {
   constructor(props) {
@@ -19,7 +24,7 @@ class CoursesPage extends React.Component {
 
   handleSubmit = (event) => {
     event.preventDefault();
-    alert(this.state.course.title);
+    this.props.actions.createCourse(this.state.course);
   };
 
   render() {
@@ -33,9 +38,40 @@ class CoursesPage extends React.Component {
           value={this.state.course.title}
         ></input>
         <input type="submit" value="Save" />
+        {this.props.courses.map((course) => (
+          <div key={course.title}>{course.title}</div>
+        ))}
       </form>
     );
   }
 }
 
-export default CoursesPage;
+CoursesPage.propTypes = {
+  courses: PropTypes.array.isRequired,
+  actions: PropTypes.object.isRequired,
+};
+
+// This function determines what state is passed to our component via props
+function mapStateToProps(state) {
+  return {
+    // Here be specific, to avoid React to reload when no needed
+    courses: state.courses,
+  };
+}
+
+// We have to pass to DISPATCH that notifies REDUX
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators(courseActions, dispatch),
+  };
+}
+
+// 2 fn calls, the connect fn return a fn and we invoke immediately this with CoursePage
+export default connect(mapStateToProps, mapDispatchToProps)(CoursesPage);
+
+/*
+També ho podriem fer sino de la següent forma en 2 linies
+const connectedStateAndProps = connect(mapStateToProps, mapDispatchToProps)
+export default connectedStateAndProps(CoursesPage)
+
+*/
--- building-apps-react-redux/src/index.js
+++ building-apps-react-redux/src/index.js
@@ -4,10 +4,17 @@ import { BrowserRouter as Router } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./components/App";
 import "./index.css";
+import configureStore from "./redux/configureStore";
+import { Provider as ReduxProvider } from "react-redux";
 
+const store = configureStore();
+
+// Provider is a high order component, we rename to use here as ReduxProvider
 render(
-  <Router>
-    <App />
-  </Router>,
+  <ReduxProvider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </ReduxProvider>,
   document.getElementById("app")
 );
