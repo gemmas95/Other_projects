b5c80488c4cc71eb3a4f4dbb2628029e90191d79
gemmas95 <gemma1030@gmail.com>
2020-09-30 12:17:48 +0200

    navigation done

--- building-apps-react-redux/src/components/App.js
+++ building-apps-react-redux/src/components/App.js
@@ -1,14 +1,22 @@
 /* eslint-disable react/jsx-no-duplicate-props */
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import HomePage from "./home/HomePage";
 import AboutPage from "./about/AboutPage";
+import Header from "./common/Header";
+import PageNotFound from "./PageNotFound";
+import CoursesPage from "./courses/CoursesPage";
 
 function App() {
   return (
     <div className="container-fluid">
-      <Route exact component path="/" component={HomePage} />
-      <Route component path="/about" component={AboutPage} />
+      <Header />
+      <Switch>
+        <Route exact component path="/" component={HomePage} />
+        <Route component path="/about" component={AboutPage} />
+        <Route component path="/courses" component={CoursesPage} />
+        <Route component={PageNotFound} />
+      </Switch>
     </div>
   );
 }
