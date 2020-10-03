eec8e61676c4a0e7fe80dc770d3d3857ddb8d989
gemmas95 <gemma1030@gmail.com>
2020-10-03 17:40:07 +0200

    added ok

--- building-apps-react-redux/src/components/courses/ManageCoursePage.js
+++ building-apps-react-redux/src/components/courses/ManageCoursePage.js
@@ -1,19 +1,33 @@
 // Section 1 - Imports
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
-import { loadCourses } from "../../redux/actions/courseActions";
+import { loadCourses, saveCourse } from "../../redux/actions/courseActions";
 import { loadAuthors } from "../../redux/actions/authorActions";
 import PropTypes from "prop-types";
+import CourseForm from "./CourseForm";
+import { newCourse } from "../../../tools/mockData";
 
 // Section 2 - Component
-class ManageCoursePage extends React.Component {
-  componentDidMount() {
-    const { courses, authors, loadAuthors, loadCourses } = this.props;
+function ManageCoursePage({
+  courses,
+  authors,
+  loadAuthors,
+  loadCourses,
+  saveCourse,
+  history,
+  // Aquest rest operator diu que les orpietats no assignades a ninguna de les anteriors les guardi a un objecte que es tigui props
+  ...props
+}) {
+  const [course, setCourse] = useState({ ...props.course });
+  const [errors, setErrors] = useState({});
 
+  useEffect(() => {
     if (courses.length === 0) {
       loadCourses().catch((error) => {
         alert("Loading courses failed" + error);
       });
+    } else {
+      setCourse({ ...props.course });
     }
 
     if (authors.length === 0) {
@@ -21,28 +35,59 @@ class ManageCoursePage extends React.Component {
         alert("Loading authors failed" + error);
       });
     }
+  }, [props.course]);
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setCourse((prevCourse) => ({
+      // parseInt convert the id in...
+      [name]: name === "authorId" ? parseInt(value, 10) : value,
+    }));
   }
 
-  render() {
-    return (
-      <>
-        <h2>Manage Course</h2>
-      </>
-    );
+  function handleSave(event) {
+    event.preventDefault();
+    saveCourse(course).then(() => {
+      history.pushState("/courses");
+    });
   }
+
+  // El [] es exacatemnte lo mismo que hacer un componentDidMount, se renderiza 1 vez
+  return (
+    <CourseForm
+      course={course}
+      errors={errors}
+      authors={authors}
+      onChange={handleChange}
+      onSave={handleSave}
+    />
+  );
 }
 
 // Section 3 - PropTypes
 ManageCoursePage.propTypes = {
+  course: PropTypes.object.isRequired,
   authors: PropTypes.array.isRequired,
   courses: PropTypes.array.isRequired,
   loadCourses: PropTypes.func.isRequired,
   loadAuthors: PropTypes.func.isRequired,
+  saveCourse: PropTypes.func.isRequired,
+  history: PropTypes.object.isRequired,
 };
 
+export function getCourseBySlug(courses, slug) {
+  return courses.find((course) => course.slug === slug) || null;
+}
+
 // Section 4 - Redux mappings (state & actions)
-function mapStateToProps(state) {
+function mapStateToProps(state, ownProps) {
+  const slug = ownProps.match.params.slug;
+  const course =
+    slug && state.courses.length > 0
+      ? getCourseBySlug(state.courses, slug)
+      : newCourse;
   return {
+    course,
     courses: state.courses,
     authors: state.authors,
   };
@@ -51,6 +96,7 @@ function mapStateToProps(state) {
 const mapDispatchToProps = {
   loadCourses,
   loadAuthors,
+  saveCourse,
 };
 
 // Section 5 - Redux connect
--- building-apps-react-redux/src/redux/actions/courseActions.js
+++ building-apps-react-redux/src/redux/actions/courseActions.js
@@ -2,12 +2,16 @@ import * as types from "./actionTypes";
 import * as courseApi from "../../api/courseApi";
 
 // Action creator, the only must is to have a type
-export function createCourse(course) {
-  return { type: types.CREATE_COURSE, course };
-}
 
 export function loadCourseSuccess(courses) {
   return { type: types.LOAD_COURSES_SUCCESS, courses };
+}
+export function createCourseSuccess(course) {
+  return { type: types.CREATE_COURSE_SUCCESS, course };
+}
+
+export function updateCourseSuccess(course) {
+  return { type: types.UPDATE_COURSE_SUCCESS, course };
 }
 
 // THUNK MIDDLEWARE, we don't need to pass dispatch as an argument, redux thunk injects dispacth
@@ -17,6 +21,21 @@ export function loadCourses() {
       .getCourses()
       .then((courses) => {
         dispatch(loadCourseSuccess(courses));
+      })
+      .catch((error) => {
+        throw error;
+      });
+  };
+}
+
+export function saveCourse(course) {
+  return function (dispatch, getState) {
+    return courseApi
+      .saveCourse(course)
+      .then((savedCourse) => {
+        course.id
+          ? dispatch(updateCourseSuccess(savedCourse))
+          : dispatch(createCourseSuccess(savedCourse));
       })
       .catch((error) => {
         throw error;
