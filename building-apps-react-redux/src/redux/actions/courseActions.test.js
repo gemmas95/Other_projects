5bd6dd100c2511fe0eeaf5944d4c36b644896829
gemmas95 <gemma1030@gmail.com>
2020-10-06 22:10:11 +0200

    course done

--- building-apps-react-redux/src/redux/actions/courseActions.test.js
+++ building-apps-react-redux/src/redux/actions/courseActions.test.js
@@ -1,7 +1,6 @@
 import * as courseActions from "./courseActions";
 import * as types from "./actionTypes";
 import { courses } from "../../../tools/mockData";
-
 import thunk from "redux-thunk";
 import fetchMock from "fetch-mock";
 import configureMockStore from "redux-mock-store";
@@ -10,36 +9,44 @@ import configureMockStore from "redux-mock-store";
 const middleware = [thunk];
 const mockStore = configureMockStore(middleware);
 
-describe("Async Actions (thunk)", () => {
-  fetchMock.mock("*", {
-    body: courses,
-    headers: { "content-type": "application/json" },
+describe("Async Actions", () => {
+  afterEach(() => {
+    fetchMock.restore();
   });
 
-  const expectedActions = [
-    { type: types.BEGIN_API_CALL },
-    { type: types.LOAD_COURSES_SUCCESS, courses },
-  ];
+  describe("Load Courses Thunk", () => {
+    it("should create BEGIN_API_CALL and LOAD_COURSES_SUCCESS when loading courses", () => {
+      fetchMock.mock("*", {
+        body: courses,
+        headers: { "content-type": "application/json" }
+      });
 
-  const store = mockStore({ courses: [] });
-  return store.dispatch(courseActions.loadCourses()).then(() => {
-    expect(store.getActions()).toEqual(expectedActions);
+      const expectedActions = [
+        { type: types.BEGIN_API_CALL },
+        { type: types.LOAD_COURSES_SUCCESS, courses }
+      ];
+
+      const store = mockStore({ courses: [] });
+      return store.dispatch(courseActions.loadCourses()).then(() => {
+        expect(store.getActions()).toEqual(expectedActions);
+      });
+    });
   });
 });
 
-describe("createCourseSuccess (action creators)", () => {
+describe("createCourseSuccess", () => {
   it("should create a CREATE_COURSE_SUCCESS action", () => {
-    // Arrange
+    //arrange
     const course = courses[0];
     const expectedAction = {
       type: types.CREATE_COURSE_SUCCESS,
-      course,
+      course
     };
 
-    // Act
+    //act
     const action = courseActions.createCourseSuccess(course);
 
-    // Expect
+    //assert
     expect(action).toEqual(expectedAction);
   });
 });
--- building-apps-react-redux/src/redux/configureStore.js
+++ building-apps-react-redux/src/redux/configureStore.js
@@ -1,5 +1,5 @@
-// Use CommonJS require below so we can dynamically import during build-time
-if (ProcessingInstruction.env.NODE_ENV === "production") {
+// Use CommonJS require below so we can dynamically import during build-time.
+if (process.env.NODE_ENV === "production") {
   module.exports = require("./configureStore.prod");
 } else {
   module.exports = require("./configureStore.dev");
