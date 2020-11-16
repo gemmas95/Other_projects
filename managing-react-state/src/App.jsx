856a3e7bd82a961e5e604a956499a8c1641cdd3e
gemmas95 <gemma1030@gmail.com>
2020-11-16 19:09:57 +0100

    added useEffect

--- managing-react-state/src/App.jsx
+++ managing-react-state/src/App.jsx
@@ -1,20 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import Footer from "./Footer";
 import Header from "./Header";
+import { getProducts } from "./services/productService";
 
 export default function App() {
-  // function renderProduct(p) {
-  //   return (
-  //     <div key={p.id} className="product">
-  //       <a href="/">
-  //         <img src={`/images/${p.image}`} alt={p.name} />
-  //         <h3>{p.name}</h3>
-  //         <p>${p.price}</p>
-  //       </a>
-  //     </div>
-  //   );
-  // }
+  const [size, setSize] = useState("");
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    getProducts("shoes").then((response) => setProducts(response));
+  }, []);
+
+  function renderProduct(p) {
+    return (
+      <div key={p.id} className="product">
+        <a href="/">
+          <img src={`/images/${p.image}`} alt={p.name} />
+          <h3>{p.name}</h3>
+          <p>${p.price}</p>
+        </a>
+      </div>
+    );
+  }
+
+  const filteredProducts = size
+    ? products.filter((p) => p.skus.find((s) => s.size === +size))
+    : products;
 
   return (
     <>
@@ -23,13 +35,19 @@ export default function App() {
         <main>
           <section id="filters">
             <label htmlFor="size">Filter by Size:</label>{" "}
-            <select id="size">
+            <select
+              id="size"
+              value={size}
+              onChange={(e) => setSize(e.target.value)}
+            >
               <option value="">All sizes</option>
               <option value="7">7</option>
               <option value="8">8</option>
               <option value="9">9</option>
             </select>
+            {size && <h2>Found {filteredProducts.length} items</h2>}
           </section>
+          <section id="products">{filteredProducts.map(renderProduct)}</section>
         </main>
       </div>
       <Footer />
