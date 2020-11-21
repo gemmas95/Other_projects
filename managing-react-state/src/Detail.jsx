788c1534570d6eea683b647bfaac75ae5a31fd9c
gemmas95 <gemma1030@gmail.com>
2020-11-21 20:28:12 +0100

    4rt module done

---
+++ managing-react-state/src/Detail.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import PageNotFound from "./PageNotFound";
+import useFetch from "./services/useFetch";
+import Spinner from "./Spinner";
+
+export default function Detail() {
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const [sku, setSku] = useState("");
+
+  const { data: product, loading, error } = useFetch(`products/${id}`);
+
+  if (loading) return <Spinner />;
+  if (!product) return <PageNotFound />;
+  if (error) throw error;
+
+  return (
+    <div id="detail">
+      <h1>{product.name}</h1>
+      <p>{product.description}</p>
+      <p id="price">${product.price}</p>
+      <select id="size" value={sku} onChange={(e) => setSku(e.target.value)}>
+        <option value="">What size?</option>
+        {product.skus.map((productSku) => {
+          return (
+            <option key={productSku.sku} value={productSku.sku}>
+              {productSku.size}
+            </option>
+          );
+        })}
+      </select>
+
+      <p>
+        <button
+          className="btn btn-primary"
+          disabled={!sku}
+          onClick={() => {
+            navigate("/cart");
+          }}
+        >
+          Add to cart
+        </button>
+      </p>
+      <img src={`/images/${product.image}`} alt={product.category} />
+    </div>
+  );
+}
--- managing-react-state/src/Header.jsx
+++ managing-react-state/src/Header.jsx
@@ -1,4 +1,12 @@
 import React from "react";
+import { Link, NavLink } from "react-router-dom";
+
+const activeStyle = {
+  color: "purple",
+};
+
+//Link and NavLink are very similar, but NavLink unlike Link provides us
+//from a activeStyle that is displayed when the route is active
 
 export default function Header() {
   return (
@@ -6,7 +14,19 @@ export default function Header() {
       <nav>
         <ul>
           <li>
-            <img alt="Carved Rock Fitness" src="/images/logo.png" />
+            <Link to="/">
+              <img alt="Carved Rock Fitness" src="/images/logo.png" />
+            </Link>
+          </li>
+          <li>
+            <NavLink activeStyle={activeStyle} to="/shoes">
+              Shoes
+            </NavLink>
+          </li>
+          <li>
+            <NavLink activeStyle={activeStyle} to="/cart">
+              Cart
+            </NavLink>
           </li>
         </ul>
       </nav>
